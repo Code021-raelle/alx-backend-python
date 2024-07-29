f34274cfe3a40b025ebe903f0ef2ef30b36ab172
Code021-raelle <gabrielakinshola3@gmail.com>
2024-07-29 14:55:30 +0100

    unittest

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -32,6 +32,26 @@ class TestGithubOrgClient(unittest.TestCase):
 
         self.assertEqual(result, expected_url)
 
+    @patch('client.get_json')
+    @patch('client.GithubOrgClient._public_repos_url', new_callable=PropertyMock)
+    def test_public_repos(self, mock_public_repos_url, mock_get_json):
+        expected_url = "https://api.github.com/orgs/google/repos"
+        mock_public_repos_url.return_value = expected_url
+
+        expected_payload = [
+            {'name': 'repo1'},
+            {'name': 'repo2'},
+            {'name': 'repo3'}
+        ]
+        mock_get_json.return_value = expected_payload
+
+        client = GithubOrgClient("google")
+        result = client.public_repos()
+
+        self.assertEqual(result, ['repo1', 'repo2', 'repo3'])
+        mock_public_repos_url.assert_called_once()
+        mock_get_json.assert_called_once_with(expected_url)
+
 
 if __name__ == "__main__":
     unittest.main()
